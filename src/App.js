132822c7e6b0402a21c208ab1bdfb978be6af9d0
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-06-28 11:41:31 +0200

    setup the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import List from "./components/List";
+import Alert from "./components/Alert";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        // React Fragments allow you to wrap or group multiple elements
+        // without adding an extra node to the DOM.
+        // This can be useful when rendering multiple child elements/components
+        //  in a single parent component.
+        <>
+            <Alert />
+            <Form />
+            <List />
+        </>
+    );
 }
 
 export default App;
